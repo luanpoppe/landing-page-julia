0a37697cb639ac8c07ccd0acefe2dc2e2905ee55
luanpoppe <luanpoppe@gmail.com>
2024-11-02 19:32:37 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Home } from "./pages/Home";
+
+function App() {
+  return <Home></Home>;
+}
+
+export default App;
---
+++ src/utils/colors.ts
@@ -0,0 +1,6 @@
+export const mainColors = {
+  purple: "#15002E",
+  white: "#ffffff",
+  blackLetters: "rgb(53, 55, 56)",
+  whiteLetters: "rgb(232, 232, 232)",
+};
