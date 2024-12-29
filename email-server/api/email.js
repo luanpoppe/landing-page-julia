03015e760ce6b7e4e2eea614f7f7178bcdfaf7d3
luanpoppe <luanpoppe@gmail.com>
2024-12-28 21:58:38 -0300

    fix

--- email-server/api/email.js
+++ email-server/api/email.js
@@ -5,6 +5,9 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all domains
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Allow specific methods
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   if (req.method === "POST") {
     const { emailFrom, name, message } = req.body;
