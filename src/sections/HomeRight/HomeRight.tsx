df3f51c30c96250baf85285d706e9bb98a3f189f
luanpoppe <luanpoppe@gmail.com>
2025-01-07 14:17:14 -0300

    refactor: melhorando seção home

--- src/sections/HomeRight/HomeRight.tsx
+++ src/sections/HomeRight/HomeRight.tsx
@@ -1,89 +1,69 @@
 import { Link } from "react-router-dom";
 import { mainColors } from "../../utils/colors";
-import styled from "styled-components";
-
-const HomeRightStyled = styled.section`
-  max-width: 1248px;
-  padding: 5%;
-  margin: 0;
-  /* width: 65%; */
-
-  @media (min-width: 992px) {
-    padding: 108px 5%;
-  }
-
-  @media (max-width: 768px) {
-    padding: 2%;
-  }
-`;
-
-const ProjectStyles = styled(Link)`
-  @media (min-width: 992px) and (max-width: 1199px) {
-    margin-bottom: 120px;
-  }
-`;
 
 export function HomeRight() {
   return (
-    <HomeRightStyled
-      className="container overflow-y-scroll overflow-x-hidden text-center row g-4 flex-column aling-items-center flex-xl-row"
+    <section
+      className="container m-0 mt-md-5 overflow-y-scroll overflow-x-hidden text-center"
       id="right-home"
     >
-      <ProjectStyles
-        className="hidden col-12 col-xl-6 h-100"
-        to={"/project/sanar"}
-      >
-        <div style={{ color: mainColors.blackLetters }}>
-          <img
-            src="./home-picture-1.png"
-            alt="Project Sanar"
-            className="grow-on-hover"
-            style={
-              {
-                // width: "100%",
-                // maxWidth: "400px",
-                // maxHeight: "390px",
-              }
-            }
-          />
-          <div className="d-flex flex-column align-items-center justify-content-center">
-            <h4
-              className="font-varela-round text-center"
-              style={{ paddingTop: "22px", width: "80%" }}
-            >
-              Rebranding for all Sanar's Landing Pages - Click to see more!
-            </h4>
-            <div>2023</div>
+      <div className="row g-4 flex-column aling-items-center flex-xl-row">
+        <Link className="hidden col-12 col-xl-6 h-100" to={"/project/sanar"}>
+          <div style={{ color: mainColors.blackLetters }}>
+            <ImageProject src="./home-picture-1.png" alt="Project Sanar" />
+
+            <TitleAndSubtitle
+              title="Rebranding for all Sanar's Landing Pages - Click to see more!"
+              subtitle="2023"
+            />
           </div>
-        </div>
-      </ProjectStyles>
-      <ProjectStyles
-        className="hidden col-12 col-xl-6 h-100"
-        to={"/project/shopping-center"}
-      >
-        <div style={{ color: mainColors.blackLetters }}>
-          <img
-            src="./home-picture-2.png"
-            alt="Project navigation for shopping centers"
-            className="grow-on-hover"
-            style={
-              {
-                // maxWidth: "400px",
-                // maxHeight: "390px",
-              }
-            }
-          />
-          <div className="d-flex flex-column align-items-center justify-content-center">
-            <h4
-              className="font-varela-round text-center"
-              style={{ paddingTop: "22px", width: "80%" }}
-            >
-              Navigation app for shopping centers - Click to see more!
-            </h4>
-            <div>2024</div>
+        </Link>
+
+        <Link
+          className="hidden col-12 col-xl-6 h-100"
+          to={"/project/shopping-center"}
+        >
+          <div style={{ color: mainColors.blackLetters }}>
+            <ImageProject
+              src="./home-picture-2.png"
+              alt="Project navigation for shopping centers"
+            />
+
+            <TitleAndSubtitle
+              title="Navigation app for shopping centers - Click to see more!"
+              subtitle="2024"
+            />
           </div>
-        </div>
-      </ProjectStyles>
-    </HomeRightStyled>
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+type ImageProjectProps = {
+  src: string;
+  alt: string;
+};
+
+function ImageProject({ alt, src }: ImageProjectProps) {
+  return <img src={src} alt={alt} className="grow-on-hover" />;
+}
+
+type TitleAndSubtitleProps = {
+  title: string;
+  subtitle: string;
+};
+
+function TitleAndSubtitle({ subtitle, title }: TitleAndSubtitleProps) {
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center">
+      <h4
+        className="font-varela-round text-center"
+        style={{ paddingTop: "22px", width: "80%" }}
+      >
+        {title}
+      </h4>
+      <div>{subtitle}</div>
+    </div>
   );
 }
